2f86602c3a74931ad2e6e770be11ad26b1bbba04
Mr.Hợp <hopvq89@gmail.com>
2023-12-28 20:40:46 +0700

    add mega menu

--- active.js
+++ active.js
@@ -10,3 +10,29 @@ function showSearchOnLeave() {
     $('.search-full').addClass('hide');
     $('.search-box').val('');
 }
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" tab-active", "");
+    }
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " tab-active";
+}
+function openStab(evt, tabName) {
+    var i, stabcontent, stablinks;
+    stabcontent = document.getElementsByClassName("stabcontent");
+    for (i = 0; i < stabcontent.length; i++) {
+        stabcontent[i].style.display = "none";
+    }
+    stablinks = document.getElementsByClassName("stablinks");
+    for (i = 0; i < stablinks.length; i++) {
+        stablinks[i].className = stablinks[i].className.replace(" tab-active", "");
+    }
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " tab-active";
+}
